450c6dbdc9142effd70ae854bcc47ae8aed92f59
Ancordss <maradiaga.l.james@gmail.com>
2022-04-28 19:40:37 -0600

    conexion con la DB

---
+++ tarea_super/tarea_super/tarea_super.cpp
@@ -0,0 +1,22 @@
+// tarea_super.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <mysql.h>
+#include "ConexionDB.h"
+using namespace std;
+
+int main(){
+    int q_estado;
+    ConexionBD cn = ConexionBD();
+    cn.abrir_conexion();
+
+    if (cn.getConectar()) {
+        cout << "Conexion Exitosa..." << endl;
+    }
+    else{ cout << "Error en la | Exitosa." << endl;
+    }
+            system("pause");
+    return 0;
+}
+
