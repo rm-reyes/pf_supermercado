843324254a8ca1c6c56a26fd4e59ad676faf94e5
JavierPrado-14 <kazurdiap@miumg.edu.gt>
2022-05-18 21:24:09 -0600

    cambio

---
+++ tarea_super/tarea_super/Proveedor.h
@@ -0,0 +1,139 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "ConexionDB.h"
+using namespace std;
+
+class Proveedor
+{
+	//atributos
+private: string proveedor, nit, direccion, telefono, var;
+	   //constructor
+public: Proveedor() {
+}
+	  Proveedor(string prove, string n, string dir, string tel, string v)
+	  {
+		  proveedor = prove;
+		  nit = n;
+		  direccion = dir;
+		  telefono = tel;
+		  var = v;
+	  }
+	  //metodos set 
+	  void setProveedor(string prove) { proveedor = prove; }
+	  void setNit(string n) { nit = n; }
+	  void setDireccion(string dir) { direccion = dir; }
+	  void setTelefono(string tel) { telefono = tel; }
+	  void setVar(string v) { var = v; }
+	  //metodos get
+	  string getProveedor() { return proveedor; }
+	  string getNit() { return nit; }
+	  string getDireccion() { return direccion; }
+	  string getTelefono() { return telefono; }
+	  string getVar() { return var; }
+
+
+	  //metodos
+	//crear
+	  void crear() {
+		  int q_estado;
+		  ConexionBD cn = ConexionBD();
+		  cn.abrir_conexion();
+		  if (cn.getConectar()) {
+			  string insert = "INSERT INTO proveedores (proveedor, nit, direccion, telefono) VALUES ('" + proveedor + "','" + nit + "','" + direccion + "','" + telefono + "')";
+			  const char* i = insert.c_str();
+			  q_estado = mysql_query(cn.getConectar(), i);
+			  if (!q_estado) {
+				  cout << "Proveedor creado" << endl;
+			  }
+			  else {
+				  cout << "Error al crear proveedor" << endl;
+			  }
+		  }
+		  else {
+			  cout << "No se pudo conectar a la base de datos" << endl;
+		  }
+		  cn.cerrar_conexion();
+	  }
+
+	  //leer 
+	  void leer() {
+		  int q_estado;
+		  ConexionBD cn = ConexionBD();
+		  MYSQL_ROW fila;
+		  MYSQL_RES* resultado;
+		  cn.abrir_conexion();
+		  if (cn.getConectar()) {
+			  cout << "****lISTADO****" << endl;
+			  string consulta = "SELECT * FROM proveedores";
+			  const char* c = consulta.c_str();
+			  q_estado = mysql_query(cn.getConectar(), c);
+			  if (!q_estado) {
+				  resultado = mysql_store_result(cn.getConectar());
+				  while (fila = mysql_fetch_row(resultado)) {
+					  cout << fila[0] << " , " << fila[1] << " , " << fila[2] << " , " << fila[3] << endl;
+				  }
+
+			  }
+			  else {
+				  cout << "Error al leer proveedores" << endl;
+			  }
+		  }
+		  else {
+			  cout << "No se pudo conectar a la base de datos" << endl;
+			  system("pause");
+		  }
+		  cn.cerrar_conexion();
+	  }
+
+	  //actualizar
+	  void actualizar() {
+		  int q_estado;
+		  ConexionBD cn = ConexionBD();
+		  cn.abrir_conexion();
+		  if (cn.getConectar()) {
+			  string insert = "UPDATE proveedores\
+					        SET proveedor = '" + proveedor + "', nit = '" + nit + "', direccion = '" + direccion + "', telefono = '" + telefono + "'\
+							WHERE	idProveedor ='" + var + "';";
+			  const char* i = insert.c_str();
+			  q_estado = mysql_query(cn.getConectar(), i);
+			  if (!q_estado) {
+				  cout << "Proveedor actualizado" << endl;
+				  system("pause");
+			  }
+			  else {
+				  cout << "Error al actualizar proveedor" << endl;
+				  system("pause");
+			  }
+		  }
+		  else {
+			  cout << "No se pudo conectar a la base de datos" << endl;
+			  system("pause");
+		  }
+		  cn.cerrar_conexion();
+		  system("pause");
+	  }
+
+
+	  //borrar 
+	  void borrar() {
+		  int q_estado;
+		  ConexionBD cn = ConexionBD();
+		  cn.abrir_conexion();
+		  if (cn.getConectar()) {
+			  string insert = "DELETE FROM proveedores WHERE idProveedor = '" + var + "';";
+			  const char* i = insert.c_str();
+			  q_estado = mysql_query(cn.getConectar(), i);
+			  if (!q_estado) {
+				  cout << "Proveedor eliminado" << endl;
+			  }
+			  else {
+				  cout << "Error al eliminar proveedor" << endl;
+			  }
+		  }
+		  else {
+			  cout << "No se pudo conectar a la base de datos" << endl;
+		  }
+		  cn.cerrar_conexion();
+	  }
+};
--- tarea_super/tarea_super/tarea_super.cpp
+++ tarea_super/tarea_super/tarea_super.cpp
@@ -1,22 +1,133 @@
-// tarea_super.cpp : Este archivo contiene la funciÃ³n "main". La ejecuciÃ³n del programa comienza y termina ahÃ­.
+// tarea_super.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
 #include <iostream>
 #include <mysql.h>
-#include "ConexionDB.h"
+#include "Proveedor.h"
+#include <string>
 using namespace std;
 
-int main(){
-    int q_estado;
-    ConexionBD cn = ConexionBD();
-    cn.abrir_conexion();
+void M_proveedor();
+void crearPro();
+void leerPro();
+void actualizarPro();
+void borrarPro();
 
-    if (cn.getConectar()) {
-        cout << "Conexion Exitosa..." << endl;
-    }
-    else{ cout << "Error en la | Exitosa." << endl;
-    }
-            system("pause");
-    return 0;
+int main() {
+	/*
+	int q_estado;
+	ConexionBD cn = ConexionBD();
+	cn.abrir_conexion();
+
+	if (cn.getConectar()) {
+		cout << "Conexion Exitosa..." << endl;
+	}
+	else{ cout << "Error en la | Exitosa." << endl;
+	}
+			system("pause");
+	return 0;
+	*/
+
+
+	M_proveedor();
 }
 
+void M_proveedor() {
+	do
+	{
+		int switch_on = 0;
+		cout << "para agregar un proveedor ingrese 1: " << endl;
+		cout << "para leer la tabla ingresa 2: " << endl;
+		cout << "para actualizar la tabla ingresa 3" << endl;
+		cout << "para borrar de la tabla ingresa 4 " << endl;
+		cout << "para salir presiona 0" << endl;
+		cin >> switch_on;
+		switch (switch_on)
+		{
+		case 1: crearPro(); break;
+
+		case 2: leerPro(); break;
+
+		case 3: actualizarPro(); break;
+
+
+		case 4: borrarPro(); break;
+
+
+		case 0: exit(-1);
+
+		default: cout << "error ingresa un caracter valido!!!!" << endl;
+			break;
+		}
+
+
+	} while (1 == 1);
+
+}
+
+void crearPro() {
+	system("cls");
+	string proveedor, nit, direccion, telefono, var;
+	getline(cin, proveedor);
+	cout << "ingrese el nombre del proveedor: " << endl;
+	getline(cin, proveedor);
+	cout << "ingrese el nit del proveedor: " << endl;
+	getline(cin, nit);
+	cout << "ingrese la direccion del proveedor: " << endl;
+	getline(cin, direccion);
+	cout << "ingrese el telefono del proveedor: " << endl;
+	getline(cin, telefono);
+
+	Proveedor p = Proveedor(proveedor, nit, direccion, telefono, var);
+	p.crear();
+	p.leer();
+
+}
+
+void leerPro() {
+	system("cls");
+
+	string proveedor, nit, direccion, telefono, var;
+	cout << "------------TABLA PROVEEDORES-----------" << endl;
+	cout << "----------------------------------------" << endl;
+
+	Proveedor p = Proveedor(proveedor, nit, direccion, telefono, var);
+	p.leer();
+}
+
+void actualizarPro() {
+	system("cls");
+	string proveedor, nit, direccion, telefono, var;
+	getline(cin, var);
+	cout << "ingrese el id del proveedor que desea actualizar: " << endl;
+	getline(cin, var);
+	cout << " ********" << endl;
+	getline(cin, proveedor);
+	cout << "ingrese el nombre del proveedor: " << endl;
+	getline(cin, proveedor);
+	cout << "ingrese el nit del proveedor: " << endl;
+	getline(cin, nit);
+	cout << "ingrese la direccion del proveedor: " << endl;
+	getline(cin, direccion);
+	cout << "ingrese el telefono del proveedor: " << endl;
+	getline(cin, telefono);
+
+	Proveedor p = Proveedor(proveedor, nit, direccion, telefono, var);
+	p.actualizar();
+	p.leer();
+
+}
+
+
+void borrarPro() {
+	system("cls");
+	string proveedor, nit, direccion, telefono, var;
+	getline(cin, proveedor);
+	cout << "ingrese el id del proveedor que desea borrar: " << endl;
+	getline(cin, var);
+
+	Proveedor p = Proveedor(proveedor, nit, direccion, telefono, var);
+	p.borrar();
+	p.leer();
+
+}
